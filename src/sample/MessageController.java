8432fc589c62d73ecb12a9a2b4e17e720c337b4d
Marco <spacegamesita@gmail.com>
2021-04-01 17:11:07 +0200

    Interfaccia

--- src/sample/MessageController.java
+++ src/sample/MessageController.java
@@ -2,22 +2,29 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class MessageController{
     private Model model;
     @FXML
     private TextArea MailText;
 
+    @FXML
+    private TextField DestinationText;
+
     public void start(Model m){
         this.model = m;
         model.currentEmailProperty().addListener((obs, oldEmail, newEmail) -> {
             if (oldEmail != null) {
                 MailText.textProperty().unbindBidirectional(oldEmail.eTextProperty());
+                DestinationText.textProperty().unbindBidirectional(oldEmail.destinationProperty());
             }
             if (newEmail== null) {
                 MailText.setText("");
+                DestinationText.setText("");
             } else {
                 MailText.textProperty().bindBidirectional(newEmail.eTextProperty());
+                DestinationText.textProperty().bindBidirectional(newEmail.destinationProperty());
             }
         });
 
